2a3cc1ef3a504c94f6cd7fdb2e81e78a61aada3d
Kirill <kirillkorolev2002@mail.ru>
2024-11-27 14:28:39 +0300

    Task_15

--- src/main/kotlin/Tasks.kt
+++ src/main/kotlin/Tasks.kt
@@ -3,3 +3,13 @@
 // дополнительное свойство studentId.
 open class Person(val name: String, var age: Int)
 class Student(name: String, age: Int, val studentId: String) : Person(name, age)
+//Создайте класс Company, который содержит
+// список сотрудников и метод для подсчета общего количества сотрудников.
+class Employee(val name: String)
+class Company {
+    private val employees = mutableListOf<Employee>()
+    fun addEmployee(employee: Employee) {
+        employees.add(employee)
+    }
+    fun totalEmployees() = employees.size
+}
