67f2598c792fc06fb1118fed5e8628f422656e53
Kirill <kirillkorolev2002@mail.ru>
2024-11-27 14:47:55 +0300

    Task_2

--- src/main/kotlin/Task_1-15.kt
+++ src/main/kotlin/Task_1-15.kt
@@ -1,3 +1,8 @@
 // Создайте класс Book, который принимает название и автора в конструкторе.
 class Book(val title: String, val author: String)
 // complete!
+/*
+Создайте класс Car,
+который имеет свойства make и model, и конструктор, принимающий эти значения.
+*/
+class Car(val make: String, val model: String)
